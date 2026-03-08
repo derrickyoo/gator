e749fd346f1b9847fb6439cbb11beefa8c5bd237
Derrick Yoo <derrickyoo@gmail.com>
2026-03-07 20:51:36 -0800

    register and run commands

---
+++ src/commands/auth.ts
@@ -0,0 +1,12 @@
+import { setUser } from "../config.js";
+
+export function handlerLogin(cmdName: string, ...args: string[]) {
+  if (args.length !== 1) {
+    throw new Error(`usage: ${cmdName} <name>`);
+  }
+
+  const userName = args[0];
+  setUser(userName);
+
+  console.log("user login successful");
+}
