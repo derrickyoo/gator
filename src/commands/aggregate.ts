01371c881cb42ba8086de0f4ad07f164ae12ee6e
Derrick Yoo <derrickyoo@gmail.com>
2026-03-08 16:44:30 -0700

    WIP: aggregate command

--- src/commands/aggregate.ts
+++ src/commands/aggregate.ts
@@ -1,12 +1,12 @@
 import { fetchFeed } from "../lib/rss.js";
 
 export async function handlerAggregate(cmdName: string, ...args: string[]) {
-  if (args.length !== 1) {
-    throw new Error(`usage: ${cmdName} <URL>`);
-  }
+  // if (args.length !== 1) {
+  //   throw new Error(`usage: ${cmdName} <URL>`);
+  // }
 
-  const feedURL = args[0];
+  const feedURL = args[0] ?? "https://www.wagslane.dev/index.xml";
   const RSSFeed = await fetchFeed(feedURL);
 
-  console.log(RSSFeed);
+  console.log(JSON.stringify(RSSFeed, null, 2));
 }
