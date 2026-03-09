bd97fdf0d87146c5378f882ac0eb431520daac7a
Derrick Yoo <derrickyoo@gmail.com>
2026-03-09 16:28:20 -0700

    scrape feeds

--- src/commands/aggregate.ts
+++ src/commands/aggregate.ts
@@ -1,3 +1,5 @@
+import { getNextFeedToFetch } from "../lib/db/queries/feeds.js";
+import type { Feed } from "../lib/db/schema.js";
 import { fetchFeed } from "../lib/rss.js";
 
 export async function handlerAggregate(cmdName: string, ...args: string[]) {
@@ -9,4 +11,27 @@ export async function handlerAggregate(cmdName: string, ...args: string[]) {
   const RSSFeed = await fetchFeed(feedURL);
 
   console.log(JSON.stringify(RSSFeed, null, 2));
+}
+
+async function scrapeFeeds() {
+  const feed = await getNextFeedToFetch();
+  if (!feed) {
+    console.log(`no feeds to fetch`);
+    return;
+  }
+  console.log(`found a feed to fetch: ${feed.name}`);
+  await scrapeFeed(feed);
+}
+
+async function scrapeFeed(feed: Feed) {
+  const feedData = await fetchFeed(feed.url);
+
+  const items = feedData.channel.item;
+  for (const item of items) {
+    console.log(`found post: %s`, item.title);
+  }
+
+  console.log(
+    `feed ${feed.name} collected, ${feedData.channel.item.length} posts found`,
+  );
 }
